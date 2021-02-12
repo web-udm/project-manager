82827b9a6243f04d398f817e20c65d087a1ea64a
Ildar Mosnabeev <web.udm@gmail.com>
2021-02-12 12:35:30 +0400

    Поправил адрес шаблона Twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,8 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
-class HomeController
-{
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('base.html.twig');
+    }
 }
