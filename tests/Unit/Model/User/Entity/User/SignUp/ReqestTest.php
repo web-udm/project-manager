bcf9823e7508f0b282513f152b07c4a3d353a27f
Ildar Mosnabeev <web.udm@gmail.com>
2021-06-09 20:00:42 +0400

    Простой тест для User

--- tests/Unit/Model/User/Entity/User/SignUp/ReqestTest.php
+++ tests/Unit/Model/User/Entity/User/SignUp/ReqestTest.php
@@ -1,7 +1,22 @@
 <?php
 
+declare(strict_types=1);
 
-class ReqestTest
+namespace App\Tests\Unit\Model\User\Entity\User\SignUp;
+
+use App\Model\User\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class RequestTest extends TestCase
 {
+    public function testSuccess(): void
+    {
+        $user = new User(
+            $email = 'test@test.test',
+            $passwordHash = 'passwordHash'
+        );
 
+        $this->assertEquals($email, $user->getEmail());
+        $this->assertEquals($passwordHash, $user->getPassword());
+    }
 }
